d458581a1e52eccdeb1e3a3b32dd69379d652662
Ininit <meininit@gmail.com>
2021-04-24 23:27:14 +0800

    chore: 1143.最长公共子序列

---
+++ 1143.最长公共子序列.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=1143 lang=javascript
+ *
+ * [1143] 最长公共子序列
+ */
+
+// @lc code=start
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function(text1, text2) {
+  const n = text1.length
+  const m = text2.length
+  const dp = new Array(n + 1).fill(0)
+              .map(() => new Array(m + 1).fill(0))
+  
+
+  for(let i = 1; i <= n; i++) {
+    for(let j = 1; j <= m; j++) {
+      if(text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+      }
+    }
+  }
+
+  return dp[n][m]
+};
+// @lc code=end
+
